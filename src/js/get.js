6cbe8b874198c3e23f923fe744df160e85e36c2c
xiguaxigua <xiguaxigua888@gmail.com>
2018-06-07 14:33:42 +0800

    fix(get): get error

--- src/js/get.js
+++ src/js/get.js
@@ -2,7 +2,7 @@ export function get (target, path, defaultValue) {
   if (!path) return target
   const pathArr = path.split('.')
   let targetTemp = target
-  pathArr.map((item, index) => {
+  pathArr.some((item, index) => {
     if (targetTemp[item] === undefined) {
       targetTemp = defaultValue
       return true
