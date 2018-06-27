82adbfa54f6e357c88629d583389b093dfdbdc79
xiguaxigua <xiguaxigua888@gmail.com>
2018-06-27 18:04:21 +0800

    fix(js): get error

--- src/js/get.js
+++ src/js/get.js
@@ -1,6 +1,6 @@
 export function get (target, path, defaultValue) {
   if (!path || !target) return target
-  const pathArr = path.split ? path.split('.') : path
+  const pathArr = path.split ? path.split('.') : [path]
   let targetTemp = target
   pathArr.some((item, index) => {
     if (targetTemp[item] === undefined) {
