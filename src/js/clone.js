f3c9fdd99723c734953ffef05289ab02554bae32
xiguaxigua <xiguaxigua888@gmail.com>
2018-07-07 19:56:30 +0800

    fix(clone): clone support more type

--- src/js/clone.js
+++ src/js/clone.js
@@ -3,6 +3,7 @@ import { isObject, isArray } from './getType'
 export function clone (v) {
   if (isObject(v)) return Object.assign({}, v)
   if (isArray(v)) return v.slice()
+  return v
 }
 
 export function cloneDeep (v) {
